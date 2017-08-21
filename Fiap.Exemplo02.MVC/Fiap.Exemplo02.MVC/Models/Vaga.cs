39686de33b557cba942d4cd495a8a946607c0c34
logonrm <logonrm@fiap.com.br>
2017-08-21 20:50:44 -0300

    Adicionado a anotacao [Dislay(Name=)]

--- Fiap.Exemplo02.MVC/Fiap.Exemplo02.MVC/Models/Vaga.cs
+++ Fiap.Exemplo02.MVC/Fiap.Exemplo02.MVC/Models/Vaga.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,10 +8,19 @@ namespace Fiap.Exemplo02.MVC.Models
 {
     public class Vaga
     {
+        [Display(Name = "Cargo:")]
         public string Cargo { get; set; }
+
+        [Display(Name ="Descrição:")]
         public string Descricao { get; set; }
+
+        [Display(Name ="Salário:")]
         public decimal Salario { get; set; }
+
+        [Display(Name ="VR:")]
         public bool ValeRefeicao { get; set; }
+
+        [Display(Name ="Nível:")]
         public string Nivel { get; set; }
 
     }
