da490aad664240763f5e3b7cb66896d9729e126b
logonrm <logonrm@fiap.com.br>
2017-08-21 20:44:37 -0300

    Usando o DropDownList - SELECT com Razor

---
+++ Fiap.Exemplo02.MVC/Fiap.Exemplo02.MVC/Controllers/VagaController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Fiap.Exemplo02.MVC.Models;
+
+namespace Fiap.Exemplo02.MVC.Controllers
+{
+    public class VagaController : Controller
+    {
+        private static List<Vaga> _banco = new List<Vaga>();
+
+        // GET: Vaga
+        [HttpGet]
+        public ActionResult Cadastrar()
+        {
+            CarregarLista();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Cadastrar(Vaga vaga)
+        {
+            _banco.Add(vaga);
+            ViewBag.msg = "Sucesso ao cadastrar!";
+            CarregarLista();
+            return View();
+        }
+
+        private void CarregarLista()
+        {
+            List<string> niveis = new List<string>();
+            niveis.Add("Junior");
+            niveis.Add("Pleno");
+            niveis.Add("Sênior");
+
+            ViewBag.opcoes = new SelectList(niveis);
+        }
+
+        public ActionResult Listar()
+        {
+            return View(_banco);
+        }
+    }
+}
---
+++ Fiap.Exemplo02.MVC/Fiap.Exemplo02.MVC/Models/Vaga.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Fiap.Exemplo02.MVC.Models
+{
+    public class Vaga
+    {
+        public string Cargo { get; set; }
+        public string Descricao { get; set; }
+        public decimal Salario { get; set; }
+        public bool ValeRefeicao { get; set; }
+        public string Nivel { get; set; }
+
+    }
+}
